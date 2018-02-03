0e5f48d5d4d7096b301b75fec098a2ce0cc0eed3
Yorickdj <yrajong@avans.nl>
2018-02-03 15:18:17 +0100

    webs2 eindsopdr

---
+++ Eindopdracht/hardware.php
@@ -0,0 +1,22 @@
+<style>
+<?php include 'electroshop.css'; ?>
+</style>
+<?php ?>
+
+<html>
+    <head>
+        <title>Electroshop! - Hardware</title>
+    </head>
+    <body>
+        <ul class="nav">
+			<li><a href="homepage.php"><img src="Images\logo.png" /></a></li>
+			<li><a href="laptop.php">Laptop</a></li>
+			<li><a href="games.php">Games</a></li>
+			<li><a href="desktop.php">Desktop</a></li>
+			<li><a href="hardware.php">Hardware</a></li>
+			<li><a href="userpage.php"><img src="Images\userlogo.png" /></a></li>	
+			<li><a href="shoppingcart.php"><img src="Images\cartlogo.gif" /></a></li>
+			<li><a href="logoutpage.php">Log uit</a></li>
+		</ul>
+    </body>
+</html>
