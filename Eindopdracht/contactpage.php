3146106f1bf242645f9cebf237cb57f66a23b6f6
Pimbosly <pim.westervoort@gmail.com>
2018-02-03 17:45:25 +0100

    contactpagepush

--- Eindopdracht/contactpage.php
+++ Eindopdracht/contactpage.php
@@ -10,21 +10,23 @@
 	
 	<body>
 		<div class="WebPage">
-			<h1>Contact</h1>
-			<div class="contacttext">
-				<p>Plaats:<br>
-				Adres:<br>
-				Postcode:<br>
-				Telefoonnr:
-			</div>
-			<div class="contacttext">
-				<p>'S Hertogenbosch <br>
-				Onderwijsboulevard 215 <br>
-				5223 DE <br>
-				088 525 7500
-			</div>
-			<div class="picturediv">
-				<img class="schoolpicture" src="https://punt.avans.nl/wp-content/uploads/2017/05/OWBgevelrenovatie-1920.jpg" alt="Avans Hogeschool">
+			<div class="contactprofile">
+				<h1>Contact</h1>
+				<div class="contacttext">
+					<p>Plaats:<br>
+					Adres:<br>
+					Postcode:<br>
+					Telefoonnr:
+				</div>
+				<div class="contacttext">
+					<p>'S Hertogenbosch <br>
+					Onderwijsboulevard 215 <br>
+					5223 DE <br>
+					088 525 7500
+				</div>
+				<div class="picturediv">
+					<img class="schoolpicture" src="https://punt.avans.nl/wp-content/uploads/2017/05/OWBgevelrenovatie-1920.jpg" alt="Avans Hogeschool">
+				</div>
 			</div>
 		</div>
 	</body>
